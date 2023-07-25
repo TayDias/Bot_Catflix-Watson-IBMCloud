2d18131da78e0f8142a42882ef249b9f98823ccb
TayDias <tay.mdias67@gmail.com>
2023-07-25 18:00:47 -0300

    bot

---
+++ README.md
@@ -0,0 +1,44 @@
+<h1 align="center">Bot Watson Assistant + Automações IBM Cloud</h1>
+
+<p>Backup do código de operação. A estrutura é voltada para a captação de leads para planos de uma ferramenta de streaming.</p>
+
+
+# Índice:
+
+* [Status do projeto](#status-do-projeto)
+* [Tecnologias](#Tecnologias)
+* [Funcionalidades](#Funcionalidades)
+* [Referências](#Arquitetura)
+
+
+# Status do projeto
+
+:construction: Em desenvolvimento :construction:
+
+
+# Tecnologias
+
+* [WatsonAssistant](https://www.ibm.com/br-pt/watson)
+* [IBMCloud](https://www.ibm.com/br-pt/cloud/free)
+
+
+# Funcionalidades
+
+* Boas vindas
+* Mostrar planos
+* Validação de CPF
+* Validação de CEP
+* Envio de e-mail com proposta
+
+
+
+# Referências
+
+Como escrever um README incrível no seu Github:
+https://www.alura.com.br/artigos/escrever-bom-readme
+
+Emoji-cheat-sheet:
+https://github.com/ikatyang/emoji-cheat-sheet/blob/master/README.md
+
+Introdução ao Watson Assistant:
+https://cloud.ibm.com/docs/assistant?topic=assistant-getting-started&locale=pt-BR
---
+++ cloud-functions/action.js
@@ -0,0 +1,184 @@
+function main({action, doc, zipCode, payload}) {
+    
+	if (action == "plans") {
+	    let planList = getPlans()
+	    
+	    return { 
+	        status: "Success",
+    	    plans: planList
+    	}
+    	
+	}
+	
+	else if (action == "validateDoc") {
+	    
+	    if(doc == null) {
+	        return { status: "Failed", message: "Valor não informado para CPF do cliente" }
+	    }
+	    
+	    return { status: "Success", validation: validateDocument(doc) }
+    	
+	}
+	
+	else if (action == "validateZC") {
+	    
+	    if(zipCode == null) {
+	        return { status: "Failed", message: "Valor não informado para CEP do cliente" }
+	    }
+	    
+	    return validateZipCode(zipCode)
+    	
+	}
+	
+	else if (action == "order") {
+	    
+	    if (payload == null) {
+	        return { status: "Failed", message: "Função em desenvolvimento" }
+	    }
+	    
+	    return sendEmail(payload)
+	    
+	}
+	
+	
+	return { status: "Failed", message: "Valor inválido para parâmetro action" }
+	
+}
+
+
+// Lista de planos
+function getPlans() {
+    return [
+        {
+            "id": 1,
+            "name": "Básico",
+    	    "description": "",
+    	    "price": "16"
+    	},
+    	{
+    	    "id": 2,
+    	    "name": "Padrão",
+    	    "description": "",
+    	    "price": "29"
+    	        
+    	},
+    	{
+    	    "id": 3,
+    	    "name": "Premium",
+    	    "description": "",
+    	    "price": "45"
+    	        
+    	}
+    ]
+    
+}
+
+// Validar CPF
+function validateDocument(text) {
+    let strCPF = text
+
+    if (strCPF.length !== 11 || !Array.from(strCPF).filter(e => e !== strCPF[0]).length) 
+      return false
+
+    let Soma = 0
+    let Resto
+
+    if (strCPF == "00000000000")
+	    return false
+        
+    for (i=1; i<=9; i++)
+	    Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i) 
+        Resto = (Soma * 10) % 11
+
+    if ((Resto == 10) || (Resto == 11)) 
+	    Resto = 0
+
+    if (Resto != parseInt(strCPF.substring(9, 10)) )
+	    return false
+
+	Soma = 0
+
+    for (i = 1; i <= 10; i++)
+       Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i)
+    
+    Resto = (Soma * 10) % 11
+
+    if ((Resto == 10) || (Resto == 11)) 
+	    Resto = 0
+
+    if (Resto != parseInt(strCPF.substring(10, 11) ) )
+        return false
+        
+    return true
+}
+
+// Validar CEP
+async function validateZipCode(cep) {
+    const axios = require('axios')
+    
+    let requestError = false
+    
+    const resp = await axios(`http://viacep.com.br/ws/${cep}/json/`).catch((error) => {
+        requestError = true
+    })
+    
+    if (requestError) {
+        return { status: "Failed", message: "CEP não localizado" }
+    }
+    
+    return {
+        status: "Success",
+        ibge: resp.data.ibge,
+        cep: resp.data.cep,
+        logradouro: resp.data.logradouro,
+        bairro: resp.data.bairro,
+        localidade: resp.data.localidade,
+        uf: resp.data.uf
+    }
+    
+}
+
+// Enviar e-mail para a proposta
+function sendEmail(payload) {
+    const mailer = require('nodemailer')
+    
+    // payload = name, cpf, zip_code, city, state, neighborhood, plan_id, plan_name
+    
+    const order = payload
+    const planList = getPlans()
+    const plan_name = planList[order.plan_id-1].name
+    
+    let requestError = false
+    
+    let subject = "Pedido de plano" + plan_name + " - " + order.name
+    let text = "O cliente " + order.name + " (CPF: " + order.cpf + ") solicitou a contratação do plano " + plan_name + "."
+    text = text + " Local de cobrança no endereço " + order.neighborhood + ", " + order.city + ", " + order.state + " - CEP " + order.zip_code + "."
+    
+    let transporter = mailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'testedev@gmail.com',
+        pass: '123456'
+      }
+    })
+    
+    let mailOptions = {
+      from: 'testedev@gmail.com',
+      to: 'testedev@gmail.com',
+      subject: subject,
+      text: text
+    }
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        requestError = true
+      }
+    })
+    
+    if (requestError) {
+        return { status: "Failed", message: "Falha no envio do e-mail com o pedido." }
+    }
+        
+    return { status: "Success", message: "E-mail com o pedido enviado." }
+    
+}
